c6aa4378b016bfa8c9f82c7d9628a535377b0b57
suzan2go <ksuzuki180@gmail.com>
2017-04-30 21:05:45 +0900

    fix device handler to use bind

--- handler/device_handler.go
+++ handler/device_handler.go
@@ -2,14 +2,20 @@ package handler
 
 import (
 	"github.com/labstack/echo"
-	"log"
 	"net/http"
 )
 
+type deviceHandlerRequest struct {
+	DeviceToken string
+}
+
 // PostDevice return Device json
 func (h *Handler) PostDevice(c echo.Context) error {
-	log.Printf(c.FormValue("deviceToken"))
-	device, err := h.DB.FindOrCreateDeviceByToken(c.FormValue("deviceToken"))
+	d := &deviceHandlerRequest{}
+	if err := c.Bind(d); err != nil {
+		return err
+	}
+	device, err := h.DB.FindOrCreateDeviceByToken(d.DeviceToken)
 	if err != nil {
 		return err
 	}

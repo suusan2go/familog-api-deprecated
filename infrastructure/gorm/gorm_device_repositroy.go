e4c88d623009bdfff16c9ffba50aea7457f2b662
suusan2go <ksuzuki180@gmail.com>
2017-12-24 23:37:13 +0900

    move device finder methods to repository

---
+++ infrastructure/gorm/gorm_device_repositroy.go
@@ -0,0 +1,23 @@
+package gorm
+
+import (
+	"github.com/suusan2go/familog-api/domain/model"
+)
+
+// DiaryRepository implemented by gorm
+type DiaryRepository struct {
+	DB *model.DB
+}
+
+// FindSubscribers find user subscribed diaries
+func (repo DiaryRepository) FindSubscribers(d *model.DiaryEntry) ([]*model.Device, error) {
+	var devices []*model.Device
+	if err := repo.DB.Where("diary_subscribers.diary_id =?", d.DiaryID).
+		Where("diary_subscribers.user_id != ?", d.UserID).
+		Joins("JOIN users ON users.id = devices.user_id").
+		Joins("JOIN diary_subscribers ON diary_subscribers.user_id = users.id").
+		Find(&devices).Error; err != nil {
+		return nil, err
+	}
+	return devices, nil
+}
